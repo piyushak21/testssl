a67ace93f43c75b2ef8c117ec7a6114844123203
piyushak21 <106967130+piyushak21@users.noreply.github.com>
2023-06-01 12:25:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({ origin: "*" }));
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "test",
+    statusCode: 200,
+  });
+});
+
+app.listen(5001, () => {
+  console.log("App is running on 5001");
+});
